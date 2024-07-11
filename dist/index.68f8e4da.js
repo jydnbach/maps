5d9aac21a957ce632c5ea3dfbd43fc9e5a287d29
jydnbach <jaydenkim95@gmail.com>
2024-07-11 13:42:17 +0900

    exploring type definition files (go to definition) google maps api init setup

--- dist/index.68f8e4da.js
+++ dist/index.68f8e4da.js
@@ -1,3 +1,12 @@
-console.log("hi");
+/// <reference types="@types/google.maps" />
+// import { User } from './User';
+// import { Company } from './Company';
+new google.maps.Map(document.getElementById("map"), {
+    zoom: 1,
+    center: {
+        lat: 0,
+        lng: 0
+    }
+});
 
 //# sourceMappingURL=index.68f8e4da.js.map
