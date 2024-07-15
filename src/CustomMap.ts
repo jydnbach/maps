54d7b5e0ad3f29568aaf1016f9c879fdce13d3cc
jydnbach <jaydenkim95@gmail.com>
2024-07-15 10:31:30 +0900

    one possible solution (catch: not scalable)

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -18,22 +18,13 @@ export class CustomMap {
     );
   }
 
-  addUserMarker(user: User): void {
+  // location is only commonality. ts infers to this only when using |
+  addMarker(mappable: User | Company): void {
     new google.maps.Marker({
       map: this.googleMap,
       position: {
-        lat: user.location.lat,
-        lng: user.location.lng,
-      },
-    });
-  }
-
-  addCompanyMarker(company: Company): void {
-    new google.maps.Marker({
-      map: this.googleMap,
-      position: {
-        lat: company.location.lat,
-        lng: company.location.lng,
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
       },
     });
   }
